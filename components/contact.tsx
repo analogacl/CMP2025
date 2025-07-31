284e70a91a3851e88c73297fb0d04184e491118e
analogacl <63505280+analogacl@users.noreply.github.com>
2025-07-31 11:41:14 -0400

    Update contact.tsx
    google map link iframe https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3329.4582267445967!2d-70.65193982430965!3d-33.437366373394674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c5a30909e7d3%3A0x1f4cc6455caccfea!2sP.%C2%BA%20Phillips%2043-7%2C%20Santiago%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses-419!2scl!4v1753976373706!5m2!1ses-419!2scl

--- components/contact.tsx
+++ components/contact.tsx
@@ -73,7 +73,7 @@ export default function Contact() {
               <div className="h-80 rounded-lg overflow-hidden shadow-md border border-gray-200">
                 <div className="relative w-full h-full">
                   <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3329.2309668465!2d-70.65273!3d-33.4375!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c5a7c02b7577%3A0x4e0f8d1a442dd196!2sPasaje%20Phillips%2015%2C%20Santiago%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses!2scl!4v1716417600000!5m2!1ses!2scl"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3329.4582267445967!2d-70.65193982430965!3d-33.437366373394674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662c5a30909e7d3%3A0x1f4cc6455caccfea!2sP.%C2%BA%20Phillips%2043-7%2C%20Santiago%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses-419!2scl!4v1753976373706!5m2!1ses-419!2scl"
                     width="100%"
                     height="100%"
                     style={{ border: 0 }}
